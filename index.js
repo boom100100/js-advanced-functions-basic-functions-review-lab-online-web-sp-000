6055156140f595fd5f4b0c2ba17515dc71d44ccd
Bernadette Davis <bsd231@nyu.edu>
2020-01-25 20:25:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ function actionApplyer(startInt, arrayFcns){
     return startInt;
   } else {
     let result;
-    for (let i = arrayFcns.length; i > 0; i--){
+    for (let i = arrayFcns.length - 1; i >= 0; i--){
       result = arrayFcns[i](startInt);
     }
     return result;
